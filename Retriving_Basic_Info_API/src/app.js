cbf89e895b1a7c1679fcfc11c7d0a345d5702983
Kosisochii1 <emmanuelezeoyirkosi@yahoo.com>
2025-01-31 21:01:58 +0100

    HNG Stage 0

--- Retriving_Basic_Info_API/src/app.js
+++ Retriving_Basic_Info_API/src/app.js
@@ -1,21 +1,30 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 
 const port = process.env.PORT;
 
 const app = express();
 
 const basic = {
-  email: "ezeoyiri92@gmail.com",
+  email: process.env.EMAIL,
   current_datetime: `${new Date().toISOString()}`,
-  github_url: "<https://github.com/yourusername/your-repo>",
+  github_url: process.env.GITHUBREPO,
 };
-
+app.use(cors());
 app.get("/", (req, res) => {
+  return res.status(200).json({ massage: "Basic Information" });
+});
+
+app.get("/basic_info", (req, res) => {
   return res.status(200).json({
     massage: "Basic Info",
     data: basic,
   });
+});
+
+app.get("*", (req, res) => {
+  return res.status(400).json({ massage: "Bad Request" });
 });
 
 app.listen(port, () => {
